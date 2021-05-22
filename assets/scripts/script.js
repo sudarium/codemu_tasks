d61b4afde77e298119e8229e53bf2bbebdd67eab
e.antonov <zhekasudarium@gmail.com>
2021-05-22 21:58:54 +0300

    add solution for task 151.1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,13 @@
-let btn = document.querySelector('.btn');
-btn.addEventListener('mouseover', function(){
-    alert ('Hi!')
+let button1 = document.querySelector('#button1');
+button1.addEventListener('click', func1);
+
+let button2 = document.querySelector('#button2');
+button2.addEventListener('click', func2);
+
+function func1() {
+	alert(1);
 }
-)
+
+function func2() {
+	alert(2);
+}
