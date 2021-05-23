4d0da045e2ee26e368f9e13fd570cd8db364b496
e.antonov <zhekasudarium@gmail.com>
2021-05-23 11:01:23 +0300

    Add solution for task 153.2

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector('.btn');
+
+button.addEventListener('dblclick', function(){
+    alert('1');
+})
+
+button.addEventListener('dblclick', function(){
+    alert('2');
+})
+
+button.addEventListener('dblclick', function(){
+    alert('3');
+})
