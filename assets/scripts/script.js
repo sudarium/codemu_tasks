36d315ea35b6f328721d264aa417c0ec5fcbedc3
e.antonov <zhekasudarium@gmail.com>
2021-05-23 18:18:36 +0300

    add solution for task 155.1

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+let input = document.querySelector('input');
+
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(){
+    alert(input.type);
+    alert(input.id);
+})
+
