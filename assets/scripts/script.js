65ee299787b1bcb687d84562be4750755ccde32b
e.antonov <zhekasudarium@gmail.com>
2021-05-24 16:32:12 +0300

    add solution for task 155.7

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+let par = document.querySelector('.text');
+let btn = document.querySelector('.btn');
+let img = document.querySelector('.img');
+
+btn.addEventListener('click', function(){
+    img.width = img.width *2;
+})
