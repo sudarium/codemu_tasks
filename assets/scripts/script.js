831438183ccd9f0bc2aed4610d95dcf64531efff
e.antonov <zhekasudarium@gmail.com>
2021-05-24 16:11:59 +0300

    add solution for task 155.5

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector('.btn');
+
+let par = document.querySelector('.text');
+
+let img = document.querySelector('img');
+
+btn.addEventListener('click', function(){
+    par.innerHTML = img.src
+})
+
