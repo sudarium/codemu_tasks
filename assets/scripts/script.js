bc4d886610c60408d230c90de0b38a328d0d719e
Evgeny <zhekaorlangur@gmail.com>
2021-06-01 17:06:26 +0300

    add solution for task 160.3

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,15 @@
-let par = document.querySelectorAll('.text')
-console.log(par1); 
+let par1 = document.querySelector('.text:nth-child(1)');
+let par2 = document.querySelector('.text:nth-child(2)');
+let par3 = document.querySelector('.text:nth-child(3)');
+let par4 = document.querySelector('.text:nth-child(4)');
+let par5 = document.querySelector('.text:nth-child(5)');
+
+par1.addEventListener('click', fun);
+par2.addEventListener('click', fun);
+par3.addEventListener('click', fun);
+par4.addEventListener('click', fun);
+par5.addEventListener('click', fun);
+
+function fun(){
+ this.innerHTML += '!'   
+}
