d366b4f9da2ed223d339cb6a554b8a2ee11cace8
e.antonov <zhekasudarium@gmail.com>
2021-06-06 14:56:55 +0300

    add solution for task 169.1

---
+++ assets/scripts/script.js
@@ -0,0 +1,6 @@
+let elem = document.querySelector('.container');
+let btn = document.querySelector('.btn')
+btn.addEventListener('click', function(){
+    elem.style.width = '150px'
+    elem.style.heigth = '10px'
+})
