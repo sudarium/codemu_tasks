a60411ac159e089934f5289b4843516985781703
e.antonov <zhekasudarium@gmail.com>
2021-06-06 16:58:31 +0300

    add solution for task 170.2

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+let btn = document.querySelectorAll('.btn');
+let par = document.querySelector('.par');
+for(let el of btn){
+    el.addEventListener('click', funcSwitch);
+}
+function funcSwitch(){
+if(this.getAttribute('data-cross') === 'cross_btn'){
+    funcCross();
+}
+else if(this.getAttribute('data-bold') === 'bold_btn'){
+    funcBold();
+}
+else if(this.getAttribute('data-colored') === 'colored_btn'){
+    funcColored();
+}
+
+}
+function funcCross(){
+   par.classList.add('cross'); 
+}
+function funcBold(){
+    par.classList.add('bold'); 
+}
+function funcColored(){
+    par.classList.add('colored');
+}
